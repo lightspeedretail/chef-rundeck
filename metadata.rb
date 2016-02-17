2559bfcd2dcc3abed7c6b8c11e51b41f86c94cac
Jean-Francois Theroux <me@failshell.io>
2016-02-17 15:05:44 -0500

    original commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rundeck'
+maintainer       'Jean-Francois Theroux'
+maintainer_email 'me@failshell.io'
+license          'MIT'
+description      'Installs/Configures rundeck'
+long_description 'Installs/Configures rundeck'
+version          '0.1.0'
