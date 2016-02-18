3d7d3ef2b015d26f18dd59adf7a06fd0d153d9de
Jean-Francois Theroux <me@failshell.io>
2016-02-18 16:58:44 -0500

    added :remove action for rundeck_user

--- resources/user.rb
+++ resources/user.rb
@@ -21,15 +21,25 @@ action :manage do
   if user?(new_resource.user, new_resource.realm_file)
     return if compare_user?(auth_line, new_resource.realm_file)
 
-    converge_by "Updating rundeck user '#{new_resource.user}'" do
+    converge_by "updating user '#{new_resource.user}'" do
       current_config = ::File.read(new_resource.realm_file)
       current_user = current_config.match(/^#{user}(.*)/).to_s
       new_config = current_config.gsub!(/#{current_user}/, auth_line)
       ::File.write(new_resource.realm_file, new_config)
     end
   else
-    converge_by "Creating user '#{new_resource.user}'" do
-      ::File.open(new_resource.realm_file, 'a').puts auth_line
+    converge_by "creating user '#{new_resource.user}'" do
+      ::File.open(new_resource.realm_file, 'a').puts "#{auth_line}\n"
     end
+  end
+end
+
+action :remove do
+  return unless user?(new_resource.user, new_resource.realm_file)
+
+  converge_by "removing user '#{new_resource.user}'" do
+    config = ::File.read(new_resource.realm_file).split("\n")
+    config.reject! { |item| item =~ /^#{user}:(.*)/ }
+    ::File.write(new_resource.realm_file, "#{config.join("\n")}\n")
   end
 end
