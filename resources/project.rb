136ad32cc9628f5d3a2fda75e5a67265e8958c23
Jean-Francois Theroux <me@failshell.io>
2016-02-23 11:36:46 -0500

    added rundeck_project resource

---
+++ resources/project.rb
@@ -0,0 +1,24 @@
+require_relative '../libraries/api'
+include RundeckAPI
+
+resource_name :rundeck_project
+
+property :description, String, required: true
+property :project, String, required: true, name_property: true
+property :properties, Hash
+property :token_file, String, default: '/etc/rundeck/tokens.properties'
+
+def whyrun_supported?
+  true
+end
+
+action :manage do
+  token = token('chef', new_resource.token_file)
+  data = { 'name' => new_resource.name, 'config' => { 'description' => new_resource.description } }
+  data['config'].merge!(new_resource.properties) if new_resource.properties
+
+  return unless project?(new_resource.project, token)
+  converge_by "creating project '#{new_resource.project}'" do
+    post('/api/15/projects', token, data.to_json)
+  end
+end
