4da918df88ccf2eecbb09ddfd081fb3b97fd0229
Jean-Francois Theroux <me@failshell.io>
2016-02-25 09:15:56 -0500

    use wait_for_rundeck_to_be_up and added remove action for project resource

--- resources/project.rb
+++ resources/project.rb
@@ -13,7 +13,10 @@ def whyrun_supported?
 end
 
 action :manage do
+  wait_for_rundeck_to_be_up
+
   token = token('chef', new_resource.token_file)
+
   data = { 'name' => new_resource.name, 'config' => { 'description' => new_resource.description } }
   data['config'].merge!(new_resource.properties) if new_resource.properties
 
@@ -21,5 +24,17 @@ action :manage do
 
   converge_by "creating project '#{new_resource.project}'" do
     post('/api/15/projects', token, data.to_json)
+  end
+end
+
+action :remove do
+  wait_for_rundeck_to_be_up
+
+  token = token('chef', new_resource.token_file)
+
+  return unless project?(new_resource.project, token)
+
+  converge_by "deleting project '#{new_resource.project}'" do
+    puts delete("/api/15/project/#{new_resource.project}", token)
   end
 end
