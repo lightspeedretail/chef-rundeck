f78eaeeefba464702312c7f3ca951dcaf7f2b33c
Alexis Vanier <alexis.vanier@lightspeedhq.com>
2017-05-16 07:48:57 -0400

    bump minor

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@failshell.io'
 license          'MIT'
 description      'Installs/Configures rundeck'
 long_description 'Installs/Configures rundeck'
-version          '99.3.1'
+version          '99.4.0'
 
 depends          'java', '~> 1.39.0'
