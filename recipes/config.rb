a411b6127ced04dcbdc430df202b9663d3e0ef12
Alexis Vanier <alexis.vanier@lightspeedhq.com>
2017-05-24 14:53:12 -0400

    DO-2471 - Upgrade rundeck to 2.8.2 (#2)
    - Upgrade Rundeck version from `2.6` to `2.8`
    - Upgrade OpenJDK version from `7` to `8`
    - Refactor Rundeck configuration file per `2.8` standards
    - Major version bump
    - Minor Rubocop pass

--- recipes/config.rb
+++ recipes/config.rb
@@ -24,7 +24,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-%w(framework realm tokens rundeck-config).each do |t|
+%w[framework realm tokens rundeck-config].each do |t|
   template "#{node['rundeck']['conf_dir']}/#{t}.properties" do
     owner node['rundeck']['user']
     group node['rundeck']['group']
@@ -35,7 +35,7 @@
   end
 end
 
-template "#{node['rundeck']['conf_dir']}/profile" do
+template '/etc/sysconfig/rundeckd' do
   owner node['rundeck']['user']
   group node['rundeck']['group']
   mode 0o400
